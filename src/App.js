73dbd7f4e657dccf6a28a000fac5b1ac54b05a5b
Alex Gaesser <agaesser@gmail.com>
2020-04-10 22:37:08 -0700

    graphql api works

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Amplify, { Analytics, Storage } from 'aws-amplify';
+import Amplify, { Analytics, Storage, API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator, S3Album } from 'aws-amplify-react';
 import awsconfig from './aws-exports';
 import '@aws-amplify/ui/dist/style.css';
 
 Amplify.configure(awsconfig);
 Storage.configure({ level: 'private' });
+
+const listTodos = `query listTodos {
+  listTodos{
+    items{
+      id
+      name
+      description
+    }
+  }
+}`;
+
+const addTodo = `mutation createTodo($name:String! $description: String!) {
+  createTodo(input:{
+    name:$name
+    description:$description
+  }){
+    id
+    name
+    description
+  }
+}`;
 
 class App extends Component {
 
@@ -25,11 +46,29 @@ class App extends Component {
     Analytics.record('Amplify_CLI');
   }
 
+  todoMutation = async () => {
+    const todoDetails = {
+      name: 'Party tonight!',
+      description: 'Amplify CLI rocks!'
+    };
+
+    const newTodo = await API.graphql(graphqlOperation(addTodo, todoDetails));
+    alert(JSON.stringify(newTodo));
+  };
+
+  listQuery = async () => {
+    console.log('listing todos');
+    const allTodos = await API.graphql(graphqlOperation(listTodos));
+    alert(JSON.stringify(allTodos));
+  };
+
   render() {
     return (
       <div className="App">
         <p> Pick a file</p>
         <input type="file" onChange={this.uploadFile} />
+        <button onClick={this.listQuery}>GraphQL Query</button>
+        <button onClick={this.todoMutation}>GraphQL Mutation</button>
         <S3Album level="private" path='' />
       </div>
     );
