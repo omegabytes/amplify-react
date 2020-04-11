0baeeeb8921ad3554b0b6e4e3d9e09f9c1cacd32
Alex Gaesser <agaesser@gmail.com>
2020-04-10 22:52:05 -0700

    swap rest for graphql

--- src/App.js
+++ src/App.js
@@ -46,29 +46,56 @@ class App extends Component {
     Analytics.record('Amplify_CLI');
   }
 
-  todoMutation = async () => {
-    const todoDetails = {
-      name: 'Party tonight!',
-      description: 'Amplify CLI rocks!'
-    };
-
-    const newTodo = await API.graphql(graphqlOperation(addTodo, todoDetails));
-    alert(JSON.stringify(newTodo));
+  post = async () => {
+    console.log('calling api');
+    const response = await API.post('reactREST', '/items', {
+      body: {
+        id: '1',
+        name: 'hello amplify!'
+      }
+    });
+    alert(JSON.stringify(response, null, 2));
+  };
+  get = async () => {
+    console.log('calling api');
+    const response = await API.get('reactREST', '/items/object/1');
+    alert(JSON.stringify(response, null, 2));
+  };
+  list = async () => {
+    console.log('calling api');
+    const response = await API.get('reactREST', '/items/1');
+    alert(JSON.stringify(response, null, 2));
   };
 
-  listQuery = async () => {
-    console.log('listing todos');
-    const allTodos = await API.graphql(graphqlOperation(listTodos));
-    alert(JSON.stringify(allTodos));
-  };
+  // todoMutation = async () => {
+  //   const todoDetails = {
+  //     name: 'Party tonight!',
+  //     description: 'Amplify CLI rocks!'
+  //   };
+  //
+  //   const newTodo = await API.graphql(graphqlOperation(addTodo, todoDetails));
+  //   alert(JSON.stringify(newTodo));
+  // };
+  //
+  // listQuery = async () => {
+  //   console.log('listing todos');
+  //   const allTodos = await API.graphql(graphqlOperation(listTodos));
+  //   alert(JSON.stringify(allTodos));
+  // };
 
   render() {
     return (
       <div className="App">
         <p> Pick a file</p>
         <input type="file" onChange={this.uploadFile} />
-        <button onClick={this.listQuery}>GraphQL Query</button>
-        <button onClick={this.todoMutation}>GraphQL Mutation</button>
+
+        {/*<button onClick={this.listQuery}>GraphQL Query</button>*/}
+        {/*<button onClick={this.todoMutation}>GraphQL Mutation</button>*/}
+
+        <button onClick={this.post}>POST</button>
+        <button onClick={this.get}>GET</button>
+        <button onClick={this.list}>LIST</button>
+
         <S3Album level="private" path='' />
       </div>
     );
